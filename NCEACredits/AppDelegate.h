ec7e6fdb2d0387411a80b1e004b9c5c29f60ec7b
Dylan Chong <DylanChong@Dylans-iMac.local>
2014-01-11 14:41:03 +1300

    Initial Commit

---
+++ NCEACredits/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NCEACredits
+//
+//  Created by Dylan Chong on 11/01/14.
+//  Copyright (c) 2014 PiGuyGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NCEACredits/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NCEACredits
+//
+//  Created by Dylan Chong on 11/01/14.
+//  Copyright (c) 2014 PiGuyGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
