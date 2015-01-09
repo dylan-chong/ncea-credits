1746fa99dc28dae5493e7a2bcb1c29b5656dc8dd
Dylan Chong <DylanChong@Dylans-iMac.local>
2015-01-09 19:17:24 +1300

    Fixed it, but about to change so that it almost fills the height of cell

---
+++ NCEACredits/ChangeColourSubjectColourTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ChangeColourSubjectColourTableViewCell.h
+//  NCEACredits
+//
+//  Created by Dylan Chong on 9/01/15.
+//  Copyright (c) 2015 PiGuyGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangeColourSubjectColourTableViewCell : UITableViewCell
+
+@property UIView *colourView;
+- (void)setColourViewColour:(UIColor *)colour;
+
+@end
